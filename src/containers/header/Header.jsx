832d2665a03df40a1f0b1f1c35ebfeadfbc8ecb2
AlexSan369 <trigramasolar@gmail.com>
2023-09-18 20:52:13 -0300

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt__header section__padding' id='home'>
+      <div className="gpt__header-content">
+        <h1 className="gradient__text">Desenvolva algo incrível usando OpenAI GPT4!</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, facere asperiores. Reiciendis itaque nulla quidem reprehenderit debitis odio? Possimus explicabo distinctio maiores excepturi esse itaque velit ipsam repellendus cumque quod.
+        </p>
+
+        <div className="gpt__header-content__input">
+          <input type="email" placeholder='Seu e-Mail' className='' />
+          <button type='button'>Comece</button>
+        </div>
+
+        <div className="gpt__header-content__people">
+          <img src={people} alt="pessoas" />
+          <p>tantas pessoas em 24 horas</p>
+        </div>
+      </div>
+
+
+      <div className="gpt__header-image">
+        <img src={ai} alt="IA" />
+      </div>
+    </div>
   )
 }
 
